706682625b8b02d9d4467d70b3d7eaa83a4ca4b5
iampeterkr <i.am.peter.kr@kaist.ac.kr>
2017-04-17 12:22:23 +0900

    shop

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL , related_name="+")
+    # user.post_set.all() 충돌을 피하기 위함 '+'는 skip
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
